5ae5d102cd5c94edfdea39b3854f1ea833a0bb82
Walid Naiji <wnaiji@gmail.com>
2023-06-28 12:12:59 +0200

    return file2 ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: walidnaiji <walidnaiji@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 13:06:39 by wnaiji            #+#    #+#             */
-/*   Updated: 2023/06/28 10:43:23 by walidnaiji       ###   ########.fr       */
+/*   Updated: 2023/06/28 12:10:04 by walidnaiji       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,18 @@ void	pipex(char **argv, char **envp)
 	arg.cmd2 = parsing_cmd(argv[3]);
 	arg.env = ft_envp(envp);
 	arg.fd_in = open_fd(argv[1]);
-	arg.fd_out = open(argv[4], O_CREAT);
+	arg.fd_out = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	pid1 = fork();
 	i = 0;
 	if (pid1 < 0)
+	{
 		perror("Error: fork pid1\n");
+		exit(EXIT_FAILURE);
+	}
 	if (pipe(fd) == -1)
+	{
 		perror("Error: pipe\n");
+		exit(EXIT_FAILURE);
 	if (pid1 == 0)
 	{
 		while (arg.env[i])
@@ -51,7 +56,6 @@ void	pipex(char **argv, char **envp)
 			str = ft_ft_strjoin(arg.env[i], arg.cmd1[0]);
 			if (access(str, X_OK) == 0)
 			{
-				printf("je passe\n");
 				close(fd[0]);
 				dup2(arg.fd_in, STDIN_FILENO);
 				close(arg.fd_in);
@@ -59,12 +63,13 @@ void	pipex(char **argv, char **envp)
 				close(fd[1]);
 				execve(str, arg.cmd1, arg.env);
 				perror("Erreur: execve\n");
+				exit(EXIT_FAILURE);
 			}
 			free(str);
 			i++;
 		}
+		exit(EXIT_SUCCESS);
 	}
-	waitpid(pid1, NULL, 0);
 	pid2 = fork();
 	if (pid2 < 0)
 		perror("Error: fork pid2\n");
@@ -73,27 +78,28 @@ void	pipex(char **argv, char **envp)
 		i = 0;
 		while (arg.env[i])
 		{
-		ft_printf("je suis ici \n");
 			str = ft_ft_strjoin(arg.env[i], arg.cmd2[0]);
 			if (access(str, X_OK) == 0)
 			{
 				close(fd[1]);
-		ft_printf("%s\n", get_next_line(fd[0]));
 				dup2(fd[0], STDIN_FILENO);
 				close(fd[0]);
 				dup2(arg.fd_out, STDOUT_FILENO);
 				close(arg.fd_out);
 				execve(str, arg.cmd2, arg.env);
 				perror("Erreur: execve\n");
+				exit(EXIT_FAILURE);
 			}
 			free(str);
 			i++;
 		}
+		exit(EXIT_SUCCESS);
 	}
 	close(fd[0]);
 	close(fd[1]);
 	close(arg.fd_in);
 	close(arg.fd_out);
+	waitpid(pid1, NULL, 0);
 	waitpid(pid2, NULL, 0);
 }
 
